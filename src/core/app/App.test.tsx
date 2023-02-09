aa3293bc2d11155de0cada8e7b65c385e1c7e03a
fernando <fernandotarraga71@gmail.com>
2023-02-09 09:39:34 +0100

    update core

--- src/core/app/App.test.tsx
+++ src/core/app/App.test.tsx
@@ -1,7 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import { Header } from '../../pages/header/header';
-test('renders learn react link', () => {
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import { Header } from "../../pages/header/header";
+testing("renders learn react link", () => {
   render(<Header />);
   const linkElement = screen.getByText(/Welcome to GOT game/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).tobeInTheDocument();
 });
+function expect(linkElement: HTMLElement) {
+  throw new Error("Function not implemented.");
+}
+function testing(arg0: string, arg1: () => void) {
+  throw new Error("Function not implemented.");
+}
